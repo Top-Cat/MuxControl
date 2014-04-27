18dd4fa46c9514be2e42f144527f9fff5ce429a3
Robert Walker <robert.walker>
2014-04-28 00:33:49 +0100

    first time wizard is getting somewhere

--- MuxControl.py
+++ MuxControl.py
@@ -27,10 +27,45 @@ import xml.etree.cElementTree as et
 
 logging.basicConfig(filename = 'MuxControl.log', level = logging.DEBUG)
 
+
+class WizPageDevice(wx.Panel):
+    
+    def __init__(self, *args, **kwargs):
+        wx.Panel.__init__(self, *args, **kwargs)
+        
+        sizer = wx.GridSizer(cols = 2)
+        nameLabel = wx.StaticText(self, label = 'Device')
+        name = wx.Choice(self)
+        hostLabel = wx.StaticText(self, label = 'Host')
+        host = wx.TextCtrl(self)
+        portLabel = wx.StaticText(self, label = 'Port')
+        port = wx.TextCtrl(self)
+        sizer.AddMany([(nameLabel), (name), (hostLabel), (host),
+                        (portLabel), (port)])
+        self.SetSizer(sizer)
+        self.SetAutoLayout(True)
+        sizer.Fit(self)
+
+
 class WizPageDeviceSetup(wxx.WizardPage):
+    
+    def onAdd(self, e):
+        
+        self.sizer.Insert(-1, WizPageDevice(self))
+        self.SetSizer(self.sizer)
+        self.sizer.Fit(self)
     
     def __init__(self, *args, **kwargs):
         wxx.WizardPage.__init__(self, *args, **kwargs)
+        devList = []
+        self.sizer = wx.BoxSizer(wx.VERTICAL)
+        text = wx.StaticText(self, label = 'Add the required devices')
+        add = wx.Button(self, label = 'Add device')
+        self.sizer.Add(add)
+        self.SetSizer(self.sizer)
+        self.SetAutoLayout(True)
+        self.sizer.Fit(self)
+        self.Bind(wx.EVT_BUTTON, self.onAdd, add)
 
 
 class WizPageWelcome(wxx.WizardPage):
@@ -38,7 +73,6 @@ class WizPageWelcome(wxx.WizardPage):
     def __init__(self, *args, **kwargs):
         wxx.WizardPage.__init__(self, *args, **kwargs)
         text = wx.StaticText(self, label = 'Welcome!\r\nLooks like you haven\'t run this before')
-        
 
 
 class FirstTime(wxx.Wizard):
@@ -46,11 +80,14 @@ class FirstTime(wxx.Wizard):
     def __init__(self, *args, **kwargs):
         wxx.Wizard.__init__(self, *args, **kwargs)
         self.addPage(WizPageWelcome(self))
+        self.addPage(WizPageDeviceSetup(self))
         self.RunWizard(self.pages[0])
 
 
 
 def main():
+    
+    availableDev = ['hub', 'mux', 'tarantula', 'txlight', 'tally'
 
     app = wx.App(False)
     
